66211ae5058a9b7ddd6af442278de632212565b9
SnowyFranzz <fran.granzier@gmail.com>
2026-05-11 21:02:39 -0300

    Initial commit

---
+++ src/client/java/snowy/snowyaddons/client/SnowyAddonsClient.java
@@ -0,0 +1,10 @@
+package snowy.snowyaddons.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SnowyAddonsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
